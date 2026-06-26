489244954444c1a693f6d3725797705d5de41ed4
matias248 <matias.andorra@gmail.com>
2026-06-26 18:40:57 +0200

    chore:rename project

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    baseUrl: "http://localhost:5173/vanillajsfrontendecommerce/#/",
+    baseUrl: "http://localhost:5173/VanillaTypeScriptFrontendEcommerce/#/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
